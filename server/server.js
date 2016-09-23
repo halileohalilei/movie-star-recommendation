7097cf3ebbf72a0ce31f2a0f013858d51d7fd5ca
halileohalilei <kayimhalil@hotmail.com>
2016-09-23 16:33:40 +0300

    added raccoon recommendation engine and its initialization

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+var raccoon = require('raccoon');
+raccoon.connect(6379, '127.0.0.1');
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -9,20 +12,50 @@ app.get('/people', function (req, res) {
   var query = req.query;
   var page = query.page;
   var per = query.per;
+  var userId = query.userId;
 
   if (typeof page == 'undefined') page = 0;
   if (typeof per == 'undefined') per = 24;
 
-  var sqlite3 = require('sqlite3').verbose();
-  var db = new sqlite3.Database('./movie_star_data.sqlite');
-
-  db.all("SELECT * from people limit " + per + " offset " + per * page, function(err, rows) {
-    //rows contain values while errors, well you can figure out.
-    res.send(rows);
+  raccoon.recommendFor(userId, per, function(results)
+  {
+    res.send(results);
   });
-  db.close();
 });
+
+var counter = 0;
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Started listening on port: 3000');
 });
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./movie_star_data.sqlite');
+
+var allConnections;
+db.all("SELECT c_user_id, c_star_id from connections", function(err, rows) {
+  //rows contain values while errors, well you can figure out.
+  allConnections = rows;
+  addNewFollow(0);
+  // for (var i = 0; i < rows.length; i++) {
+    // debugger;
+    // var follows = rows[i];
+    // console.log(follows['c_user_id'] + " follows " + follows['c_star_id']);
+    // res.send(follows);
+
+    // if (i % 1000 == 0)
+    //   console.log(i);
+  // }
+  // console.log('raccoon setup complete');
+  // res.send(rows);
+});
+db.close();
+
+function addNewFollow(row) {
+  var follows = allConnections[row];
+  raccoon.liked(follows['c_user_id'], follows['c_star_id'], function(){
+    // console.log(follows['c_user_id'] + " follows " + follows['c_star_id']);
+    if (row < 100)
+      addNewFollow(row+1);
+  });
+}
